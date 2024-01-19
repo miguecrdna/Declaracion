4406f7f6463840edffa40b41904466aea18a0b86
miguecrdna <157095183+miguecrdna@users.noreply.github.com>
2024-01-19 01:39:49 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ yesBtn.addEventListener('click',function () {
 const noBtn = document.querySelector('#noBtn');
 
 noBtn.addEventListener('mouseover',function () {
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
+    const randomX = parseInt(Math.random()*150);
+    const randomY = parseInt(Math.random()*150);
     noBtn.style.setProperty('top',randomY+'%');
     noBtn.style.setProperty('left',randomX+'%');
     noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
